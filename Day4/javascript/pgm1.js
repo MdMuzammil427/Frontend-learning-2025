157911889184aa604965ed73e650b5bb64ff43cd
MdMuzammil427 <muzammil88616@gmail.com>
2025-10-14 14:56:05 +0530

    Add files via upload

---
+++ Day4/javascript/pgm1.js
@@ -0,0 +1,24 @@
+ var msg="I am Muzammil";
+        console.log(msg);
+
+        let mssg="I am mohammed";
+        console.log(mssg);
+
+        const greeting={
+                text:"Welcome!",
+                sender:"System"
+
+            };
+            console.log(greeting);
+
+        function Hellofunction(){
+            console.log('hello from function');
+        };
+        Hellofunction();
+        helloArrowFunction=()=>{
+            console.log('Hello from Arrow Function');
+        
+        };
+         helloArrowFunction(); 
+        
+
