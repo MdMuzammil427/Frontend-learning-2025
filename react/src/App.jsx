a2becf194633a70bc881b952653c5d4c2dc85034
MdMuzammil427 <muzammil88616@gmail.com>
2025-10-16 11:47:59 +0530

    Add files via upload

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+const App=() =>{
+  return (
+    <>
+      <Header />{" "}
+      <div>
+        <h1>Hello from React</h1>
+        <p> i am Muzammil, from CSE </p>
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+constHeader = () => {
+    return(
+        <header>
+            <h1>This is my Header</h1>
+        </header>
+    )
+}
+export default Header;
